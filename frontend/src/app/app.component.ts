27f6150dd6ac31c712b839efb41fcaa3b49a4b87
Hernan Mauriccio Berrospi Reyes <mauriccioreyes@gmail.com>
2026-01-14 12:20:21 -0500

    feat(frontend): Agregue una consulta GraphQL básica para ventas usando Apollo-Angular

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Apollo, gql } from 'apollo-angular';
+
+const GET_SALES = gql`
+  query {
+    sales {
+      id
+      category
+      brand
+      amount
+    }
+  }
+`;
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent implements OnInit {
+
+  sales: any[] = [];
+
+  constructor(private apollo: Apollo) {}
+
+  ngOnInit() {
+    this.apollo
+      .watchQuery<any>({ query: GET_SALES })
+      .valueChanges
+      .subscribe(result => {
+        this.sales = result.data.sales;
+        console.log(this.sales);
+      });
+  }
 }
